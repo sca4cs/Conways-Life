627a7708f4b1f5a3fd11cbf90f09736d85378232
sca4cs <shannon.atkinson.ca@gmail.com>
2018-12-13 09:35:52 -0500

    Updates

---
+++ precache-manifest.e8ee0afefa76362fcdaa07359ae8c417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "aea46770cc0db078603b",
+    "url": "/Conways-Life/static/js/main.aea46770.chunk.js"
+  },
+  {
+    "revision": "e130c10b169b6631f3da",
+    "url": "/Conways-Life/static/js/1.e130c10b.chunk.js"
+  },
+  {
+    "revision": "aea46770cc0db078603b",
+    "url": "/Conways-Life/static/css/main.f7783f84.chunk.css"
+  },
+  {
+    "revision": "def8f352b5961848982f96928fb7adaf",
+    "url": "/Conways-Life/index.html"
+  }
+];
